c77e8fbaf2a602c9ae0a03a120377fcc052cbdb6
Abhishek <mallepalliabhishek@gmail.com>
2020-05-26 20:18:14 -0700

    added default circleci config.yml file, added .env file which is empty for now added .env to gitignore added __pycache__ to gitignore added the pointless test to make sure circleci is working properly

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class UselessTestCase(TestCase):
+    ##runs each subclass of TestCase in isolation
+    def setUp(self):
+        ##set up the test here
+        return
+    def test_pointless(self):
+        ##by default, the unittest constructs 
+        #the testing suite with all functions that begin with test
+        self.assertEqual(1,1)
